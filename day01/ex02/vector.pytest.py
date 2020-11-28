51648c3d735a08791bcef85a221f1a558434a27e
pde-bakk <pde-bakk@student.codam.nl>
2020-11-28 20:15:56 +0100

    I can write vectors in c++ and python, whoop whoop

--- day01/ex02/vector.pytest.py
+++ day01/ex02/vector.pytest.py
@@ -1,43 +1,102 @@
 
 class Vector:
 	def __init__(self, arg):
-		if isinstance(arg, list):
-			self.items = arg
-		elif isinstance(arg, int):
-			i = 0
-			self.items = list()
-			while i < arg:
-				self.items.append(float(i))
-				i += 1
+		self.values = list()
+		if isinstance(arg, int):
+			for i in range(arg):
+				self.values.append(float(i))
 		elif isinstance(arg, tuple):
-			# print("its a tuple")
-			begin = arg[0]
-			end = arg[1]
-			self.items = list()
-			while begin < end:
-				self.items.append(float(begin))
-				begin += 1
+			for i in range(arg[0], arg[1]):
+				self.values.append(float(i))
+		elif isinstance(arg, list) and isinstance(arg[0], float):
+			self.values = arg
+		self.size = len(self.values)
 
-	def __add__(self, add):
-		for i in range(len(self.items)):
-			self.items[i] += add
-		return self.items
+	def print(self):
+		print("(Vector ", self.values, ")", sep="")
 
-	def __sub__(self, sub):
-		for i in range(len(self.items)):
-			self.items[i] -= sub
-		return self.items
+	def __add__(self, other):
+		try:
+			out = list()
+			if type(other) == int:
+				for i in self.values:
+					out.append(i + other)
+				return Vector(out)
+			if len(self.values) != len(other.values):
+				raise IndexError
+			for i in range(self.size):
+				out.append(self.values[i] + other.values[i])
+			return Vector(out)
+		except IndexError:
+			print("Error. Vectors are not of equal length")
+
+	def __radd__(self, other):
+		return self + other
+
+	def __sub__(self, other):
+		try:
+			out = list()
+			if type(other) == int:
+				for i in self.values:
+					out.append(i - other)
+				return Vector(out)
+			if len(self.values) != len(other.values):
+				raise IndexError
+			for i in range(self.size):
+				out.append(self.values[i] - other.values[i])
+			return Vector(out)
+		except IndexError:
+			print("Error. Vectors are not of equal length")
+
+	def __rsub__(self, other):
+		return self - other
+
+	def __truediv__(self, other):
+		try:
+			out = list()
+			for n in self.values:
+				out.append(n / other)
+			return out
+		except ValueError:
+			print("Error. Whats going on here")
+
+	def __rtruediv__(self, other):
+		return self / other
+
+	def __mul__(self, other):
+		try:
+			out = list()
+			for n in self.values:
+				out.append(n * other)
+			return out
+		except ValueError:
+			print("Error. Whats going on here")
+
+	def __rmul__(self, other):
+		return self * other
 
 	def __str__(self):
-		return "Vector " + str(self.items)
+		out = "(Vector "
+		out += str(self.values)
+		return out + ")"
 
-peervec = Vector([0.0, 1.0, 2.0, 3.0])
-print(str(peervec))
+	def __repr__(self):
+		out = "{" + "values: " + str(self.values) \
+			+ ", size: " + str(self.size) + "}"
+		return out
 
-peervec2 = Vector(4)
-print(peervec2)
 
-peervec3 = Vector((10, 15))
-peervec3 += 5
-print(peervec3)
+V1 = Vector([0.0, 1.0, 2.0, 3.0])
+V2 = Vector(3)
+V3 = Vector((10, 15))
 
+V1.print()
+V2.print()
+V3.print()
+
+print(V1 + V2)
+print(V2 - V3)
+print(V1 - V1)
+print(V3 * 3)
+print(V2 / 2)
+print(str(V2))
